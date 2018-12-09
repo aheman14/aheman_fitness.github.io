c0acbfd069fb4b44a6477dffdf57926eb42a3b95
aheman14 <45742634+aheman14@users.noreply.github.com>
2018-12-09 14:39:35 -0800

    1.0

---
+++ scripts/myFunctions.js
@@ -0,0 +1,9 @@
+/* Alexandra Heman
+11/24/18
+My Function List
+*/
+
+//Thank you alert
+function tyAlert() {
+	alert("Thank you for your interest in our fitness club. We will contact you soon.");
+}
